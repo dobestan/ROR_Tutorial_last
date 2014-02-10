5b85145bb898ab97678047def3a9fa356a06e435
dobestan <dobestan@gmail.com>
2014-02-10 13:18:07 +0000

    Make a basic User model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+  before_save do
+    self.email = email.downcase
+  end
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true,
+                    length: { maximum: 50 }
+  validates :email, presence: true,
+                    length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX},
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+                        length: { minimum: 6 }
+  
+  has_secure_password
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,108 @@
+require 'spec_helper'
+
+describe User do
+  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
+
+  describe "User Name" do
+    describe "when name is not present" do
+      before { @user.name = " " }
+      it { should_not be_valid }
+    end
+
+    describe "when name is too long" do
+      before { @user.name = "a" * 51 }
+      it { should_not be_valid }
+    end
+  end
+
+  describe "User Email" do
+    describe "when email is not present" do
+      before { @user.email = " " }
+      it { should_not be_valid }
+    end
+
+    describe "when email is too long" do
+      before { @user.email = "a" * 51 }
+      it { should_not be_valid }
+    end
+
+    describe "Email Validation" do
+      context "when email format is invalid" do
+        it "should be invalid" do
+          emails = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar foo@bar+baz.com]
+          emails.each do |invalid_email|
+            @user.email = invalid_email
+            expect(@user).not_to be_valid
+          end
+        end
+      end
+
+      context "when email format is valid" do
+        it "should be valid" do
+          emails = %w[user@foo.com first.last@foo.jp a+b@baz.cn]
+          emails.each do |valid_email|
+            @user.email = valid_email
+            expect(@user).to be_valid
+          end
+        end
+      end
+
+      context "when email is already taken" do
+        before do
+          user_with_same_email = @user.dup
+          user_with_same_email.email = @user.email.upcase
+          user_with_same_email.save
+        end
+
+        it { should_not be_valid }
+      end
+    end
+  end
+
+  describe "User Password" do
+    describe "when password is not present" do
+      before do
+        @user = User.new(name: "Example User", email: "user@example.com", password: " ", password_confirmation: " ")
+      end
+
+      it { should_not be_valid }
+    end
+
+    describe "when password is too short" do
+      before do
+        @user.password = "a"*5
+        @user.password_confirmation = "a"*5
+      end
+
+      it { should be_invalid }
+    end
+
+    describe "return value of authenticate method" do
+      before do
+        @user.save
+      end
+      let(:found_user) { User.find_by(email: @user.email ) }
+
+      context "with valid password" do
+        it { should eq found_user.authenticate(@user.password)}
+      end
+
+      context "with invalid password" do
+        let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+        it { should_not eq user_for_invalid_password }
+        specify { expect(user_for_invalid_password).to be_false }
+      end
+    end
+  end
+end
