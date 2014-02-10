9f36a70e16fe91c92d777d01fd2310619b66ad3e
dobestan <dobestan@gmail.com>
2014-02-10 08:42:10 +0000

    update titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  # Home Page
   describe "Home Page" do
     it "should repond to http get" do
       get static_pages_home_path
@@ -11,8 +13,14 @@ describe "StaticPages" do
       visit "/static_pages/home"
       expect(page).to have_title('Home')
     end
+
+    it "should have the base title" do
+      visit "/static_pages/home"
+      expect(page).to have_title "Ruby on Rails Tutorial Sample App"
+    end
   end
 
+  # Help Page
   describe "Help Page" do
     it "should respond to http get" do
       get static_pages_help_path
@@ -23,8 +31,14 @@ describe "StaticPages" do
       visit "/static_pages/help"
       expect(page).to have_title "Help"
     end
+
+    it "should have the base title" do
+      visit "/static_pages/help"
+      expect(page).to have_title "Ruby on Rails Tutorial Sample App"
+    end
   end
 
+  # About Page
   describe "About Page" do
     it "should respond to http get" do
       get static_pages_about_path
@@ -34,6 +48,11 @@ describe "StaticPages" do
     it "should have the title 'About'" do
       visit "/static_pages/about"
       expect(page).to have_title "About"
+    end
+
+    it "should have the base title" do
+      visit "/static_pages/about"
+      expect(page).to have_title "Ruby on Rails Tutorial Sample App"
     end
   end
 
